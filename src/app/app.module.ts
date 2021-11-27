3757ec859fd1a5af066329356914d60c5c4b8f92
David Weidenbenner <44938475+comic-man@users.noreply.github.com>
2021-11-27 12:52:37 -0600

    d

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Home/Home.component';
+import { LoginComponent } from './Home/Login/Login.component';
+import { SignComponent } from './Home/Sign/Sign.component';
 import { NavComponent } from './Nav/Nav.component';
 
 
 @NgModule({
-  declarations: [	
+  declarations: [
     AppComponent,
       HomeComponent,
+      SignComponent,
+      LoginComponent,
       NavComponent
    ],
   imports: [
