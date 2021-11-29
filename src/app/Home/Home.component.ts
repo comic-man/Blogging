ff6884a7e69df2c190e5008310f8acae4c46dbca
David Weidenbenner <44938475+comic-man@users.noreply.github.com>
2021-11-29 10:21:38 -0600

    k

--- src/app/Home/Home.component.ts
+++ src/app/Home/Home.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-Home',
-  templateUrl: './Home.component/html' ,
+  template: './Home.component.html' ,
   styleUrls: ['./Home.component.css']
 })
 export class HomeComponent implements OnInit {
